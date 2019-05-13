b3186046e0e2314bf8e23be89c3c57fd88ccf6c9
frumsy <liambritt3@gmail.com>
2019-05-13 15:11:52 -0400

    add player moves and winning calc function for 3x3 board

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '口',
-        };
-    }
-
-  render() {
+function Square(props){
     return (
       <button className="square"
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
+        onClick={() => props.onClick()}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
@@ -54,7 +45,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + this.state.playerMove;
 
     return (
       <div>
@@ -102,3 +93,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for(let i=0; i<lines.length;i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ){return squares[a];}
+    }
+    return null;
+}
